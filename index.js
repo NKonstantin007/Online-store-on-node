5f32a5aaa3cf3cddc63e81330ab20ea6a2da7185
kostya007 <kostya.1998.nosarev@mail.ru>
2019-09-06 23:16:37 +0700

    Add a course in card

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const exhbs = require('express-handlebars');
 const homeRoutes = require('./routes/home');
 const coursesRoutes = require('./routes/courses');
 const addRoutes = require('./routes/add');
+const cardRoutes = require('./routes/card');
 
 const app = express();
 
@@ -25,6 +26,7 @@ app.use(express.urlencoded({extended: true}));
 app.use('/', homeRoutes);
 app.use('/courses', coursesRoutes);
 app.use('/add', addRoutes);
+app.use('/card', cardRoutes);
 
 const PORT = process.env.PORT || 3000
 
